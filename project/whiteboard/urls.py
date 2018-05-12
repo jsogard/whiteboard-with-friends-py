a732838d9b49f0dd31c959b66713c006e3a3b736
Joe Sogard <jmsogard@gmail.com>
2018-05-12 16:12:51 -0500

    start gallery; create fake board data

--- project/whiteboard/urls.py
+++ project/whiteboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('gallery/', views.gallery, name='gallery'),
 ]
--- project/whiteboard/views.py
+++ project/whiteboard/views.py
@@ -6,41 +6,41 @@ from django.http import HttpResponse
 colorSchemes = {\
 		"feather":{\
 			"background":	"#FFFDF8",\
-			"frame":		"url(../static/patterns/upfeathers.png)",\
+			"frame":		"url(/static/patterns/upfeathers.png)",\
 			"border":		"#EEE9DE",\
 			"text":			"#1E1D1C",\
 			"accent":		"#EEE9DE"\
 		},\
 		"sakura":{\
 			"background":	"#FFF8F5",\
-			"frame":		"url(../static/patterns/sakura.png)",\
+			"frame":		"url(/static/patterns/sakura.png)",\
 			"border":		"#FFF7D7",\
 			"text":			"#403936",\
 			"accent":		"#DFC7BC"\
 		},\
 		"cloudy":{\
-			"background":	"url(../static/patterns/cloudy-day.png)",\
+			"background":	"url(/static/patterns/cloudy-day.png)",\
 			"frame":		"#D8DEEC",\
 			"border":		"#BDCFCE",\
 			"text":			"#363B3B",\
 			"accent":		"#ACB6B5"\
 		},\
 		"xv":{\
-			"background":	"url(../static/patterns/xv.png)",\
+			"background":	"url(/static/patterns/xv.png)",\
 			"frame":		"#EAEAEA",\
 			"border":		"#CDCDCD",\
 			"text":			"#585858",\
 			"accent":		"#CDCDCD"\
 		},\
 		"escheresque":{\
-			"background":	"url(../static/patterns/escheresque_dark.png)",\
+			"background":	"url(/static/patterns/escheresque_dark.png)",\
 			"frame":		"#656565",\
 			"border":		"#4B4B4B",\
 			"text":			"#E5E5E5",\
 			"accent":		"#656565"\
 		},\
 		"restaurant":{\
-			"background":	"url(../static/patterns/restaurant.png)",\
+			"background":	"url(/static/patterns/restaurant.png)",\
 			"frame":		"#95CFCD",\
 			"border":		"#75AFAD",\
 			"text":			"#324B4A",\
@@ -48,8 +48,49 @@ colorSchemes = {\
 		},\
 	}
 
+testBoards = \
+[\
+	{\
+		'title': 	'feather',	\
+		'owner': 	'owner0',	\
+		'scheme': 	'feather',	\
+	},\
+	{\
+		'title': 	'sakura',	\
+		'owner': 	'owner1',	\
+		'scheme': 	'sakura',	\
+	},\
+	{\
+		'title': 	'cloud',	\
+		'owner': 	'owner2',	\
+		'scheme': 	'cloudy',	\
+	},\
+	{\
+		'title': 	'xv',	\
+		'owner': 	'owner3',	\
+		'scheme': 	'xv',	\
+	},\
+	{\
+		'title': 	'escher',	\
+		'owner': 	'owner4',	\
+		'scheme': 	'escheresque',	\
+	},\
+	{\
+		'title': 	'restaurant',	\
+		'owner': 	'owner5',	\
+		'scheme': 	'restaurant',	\
+	},\
+]
+
+
 def index(request):
 	return render(request, 'whiteboard.html', { 'scheme': colorSchemes['restaurant'] })
 
 def gallery(request):
-	return render(request, 'gallery.html', {})
+	boardData = []
+	for board in testBoards:
+		b = board.copy()
+		b['frame'] = colorSchemes[b['scheme']]['frame']
+		b['border'] = colorSchemes[b['scheme']]['border']
+		boardData.append(b)
+	return render(request, 'gallery.html', { 'boards': boardData })
