c82bf7f28edd88e67cd8f455182143c3347e8805
Joe Sogard <jmsogard@gmail.com>
2018-05-12 14:08:17 -0500

    modularize color schemes, add scheme

--- project/whiteboard/views.py
+++ project/whiteboard/views.py
@@ -3,6 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+colorSchemes = {\
+		"feather":{\
+			"background":	"#FFFDF8",\
+			"frame":		"url(../static/upfeathers.png)",\
+			"border":		"#EEE9DE",\
+			"text":			"#1E1D1C",\
+			"accent":		"#EEE9DE"\
+		},\
+		"sakura":{\
+			"background":	"#FFF8F5",\
+			"frame":		"url(../static/sakura.png)",\
+			"border":		"#FFF7D7",\
+			"text":			"#403936",\
+			"accent":		"#DFC7BC"\
+		},\
+	}
 
 def index(request):
-	return render(request, 'whiteboard.html', { 'text': 'hello poop' })
+	return render(request, 'whiteboard.html', { 'scheme': colorSchemes['sakura'] })
